4346df7611263256a61e8704b3d8028ad7175db9
Saurabh pande <93812514+Sourabhpande532@users.noreply.github.com>
2023-12-21 19:55:21 +0530

    Event-selectinng the element via js

---
+++ index.js
@@ -0,0 +1,6 @@
+
+const initialPrice = document.querySelector("#initial-price");
+const stockQuantity = document.querySelector("#stock-quantity");
+const currentPrice = document.querySelector("#current-price");
+const buttonCalculate = document.querySelector("#btn");
+const result = document.querySelector(".output");
