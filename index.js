1660562c3734de37cccbdafd51267a49c056bb57
Saurabh pande <93812514+Sourabhpande532@users.noreply.github.com>
2023-12-21 20:07:51 +0530

    ----build logic for next procedure

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ const stockQuantity = document.querySelector("#stock-quantity");
 const currentPrice = document.querySelector("#current-price");
 const buttonCalculate = document.querySelector("#btn");
 const result = document.querySelector(".output");
+
+
+function calculateProfitAndLoss(initial,quantity,current){
+if(initial>current){
+// build logic for loss 
+}else if(current>initial){
+// build logic for profit
+
+}else{
+// rest of code logic  
+}
+
+}
+calculateProfitAndLoss();
