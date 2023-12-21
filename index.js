dee8c5901bea1d45bcdda8839ad036a533068a50
Saurabh pande <93812514+Sourabhpande532@users.noreply.github.com>
2023-12-21 20:43:06 +0530

    review - As of now play arround with console

--- index.js
+++ index.js
@@ -9,12 +9,21 @@ const result = document.querySelector(".output");
 function calculateProfitAndLoss(initial,quantity,current){
 if(initial>current){
 // build logic for loss 
+let loss = (initial-current)*quantity;
+let lossPercentage = (loss/initialPrice)*100;
+let totalPrice = quantity * current;
+console.log(`The loss occured here is ${loss} by ${lossPercentage} % On investing Rs ${totalPrice}`);
 }else if(current>initial){
 // build logic for profit
-
+let profit = (current-initial)*quantity;
+let profitPercentage = (profit/initial)*100
+let totalPrice = quantity * current;
+console.log(`The profit occured here is ${profit} by ${profitPercentage} % On investing Rs ${totalPrice}`);
 }else{
 // rest of code logic  
+console.log("No profit No loss ");
 }
 
 }
-calculateProfitAndLoss();
+calculateProfitAndLoss(5,6,40);
+calculateProfitAndLoss(15,6,12);
