2820a1e0652e3ed75fc12df0db11e8ae0f78ac47
Saurabh pande <93812514+Sourabhpande532@users.noreply.github.com>
2023-12-29 18:29:07 +0530

    feat - featuring via by putting checks

--- index.js
+++ index.js
@@ -5,33 +5,42 @@ const submitButtonCalculate = document.querySelector("#btn");
 const result = document.querySelector(".output");
 
 submitButtonCalculate.addEventListener("click", submitHandler);
+
 function submitHandler() {
   let cp = Number(initialPrice.value);
   let qty = Number(stockQuantity.value);
   let sp = Number(currentPrice.value);
-  
-  calculateProfitAndLoss(cp,qty,sp);
+
+  if (isNaN(cp) || isNaN(qty) || isNaN(sp) || cp <= 0 || qty <= 0 || sp <= 0) {
+    showOutput("Please enter valid and positive values for all fields");
+  } else {
+    calculateProfitAndLoss(cp, qty, sp);
+  }
 }
 
 function calculateProfitAndLoss(initial, quantity, current) {
   if (initial > current) {
-    // build logic for loss
-    let loss = (initial - current) * quantity;
-    let lossPercentage = (loss / initialPrice) * 100;
+    // logic for loss
+    let loss = ((initial - current) * quantity).toFixed(2);
+    let lossPercentage = ((loss / (initial * quantity)) * 100).toFixed(2);
     let totalPrice = quantity * current;
-    console.log(
-      `The loss occured here is ${loss} by ${lossPercentage} % On investing Rs ${totalPrice}`
+    showOutput(
+      `The loss incurred is Rs ${loss} by ${lossPercentage}% on investing Rs ${totalPrice}`
     );
   } else if (current > initial) {
-    // build logic for profit
-    let profit = (current - initial) * quantity;
-    let profitPercentage = (profit / initial) * 100;
+    // logic for profit
+    let profit = ((current - initial) * quantity).toFixed(2);
+    let profitPercentage = ((profit / (initial * quantity)) * 100).toFixed(2);
     let totalPrice = quantity * current;
-    console.log(
-      `The profit occured here is ${profit} by ${profitPercentage} % On investing Rs ${totalPrice}`
+    showOutput(
+      `The profit gained is Rs ${profit} by ${profitPercentage}% on investing Rs ${totalPrice}`
     );
   } else {
-    // rest of code logic
-    console.log("No profit No loss ");
+    // no profit, no loss
+    showOutput("No profit, No loss");
   }
+}
+
+function showOutput(message) {
+  result.innerHTML = message;
 }
